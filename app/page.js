f9992e6f4b3a33ebdba47c7c96bcb8b4b76f1115
carlos camacho <carlic.dev@gmail.com>
2023-10-11 18:15:28 -0600

    fixed loading error

--- app/page.js
+++ app/page.js
@@ -1,18 +1,20 @@
 
 import Hero from './components/Hero'
 import Featured from './components/Featured'
-import Tags from './components/Tags'
+
 import Posts from './components/Posts'
-import CTAContact from './components/CTAContact'
 
 import { getPosts } from './services/graphql'
-import LastPostsCard from './components/LastPostsCard'
-import Subscribe from './components/Subscribe'
+
 
 export default async function Home() {
   const posts = await getPosts();
-  posts.reverse()
 
+  if (!posts) {
+    return <p>Loading...</p>
+  }
+
+  posts.reverse();
   return (
     <main className="w-full flex flex-col gap-4">
       <Hero />
